0505dce5f4da44be497adabfe3db2d6f9002d1c4
vanredni2 <vanredni2@ITCRS.edu>
2024-03-07 11:44:00 +0100

    Add project files.

---
+++ vezbe1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vezbe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ime;
+            string prezime;
+            string grad;
+
+            //Ispis korisniku
+            Console.WriteLine("Unesite ime:");
+            
+
+
+            //Iscitavanje sa konzole
+
+            ime = Console.ReadLine();
+
+            Console.WriteLine("Unesite prezime:");
+            prezime = Console.ReadLine();
+
+            Console.WriteLine("Unesite grad:");
+            grad = Console.ReadLine();
+        }
+    }
+}
